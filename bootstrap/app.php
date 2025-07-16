50e216a35430592d8b574abdb04ab4f531e4abba
Muhammad Zidan Ramadhan <147800862+rama1581@users.noreply.github.com>
2025-07-16 10:43:35 +0700

    release

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,32 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Tambahkan semua middleware global di sini
+        $middleware->append(HandleCors::class);
+        $middleware->append(EnsureFrontendRequestsAreStateful::class);
+        $middleware->append(EncryptCookies::class);
+        $middleware->append(AddQueuedCookiesToResponse::class);
+        $middleware->append(StartSession::class);
+        $middleware->append(ShareErrorsFromSession::class);
+        $middleware->append(VerifyCsrfToken::class);
+        $middleware->append(SubstituteBindings::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
